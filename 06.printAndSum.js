d1a66106ff6aee83331a5909e7c82bf49a4ae505
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-06 16:07:11 +0200

    Add files via upload

---
+++ 06.printAndSum.js
@@ -0,0 +1,14 @@
+function solve(startNum,endNum){
+
+    let sum = 0;
+    let output = ` `;
+
+    for(let i = startNum; i <= endNum;i++){
+        sum += i;
+        output +=`${i} `
+        
+    }
+    console.log(output)
+    console.log(`Sum: ${sum}`);
+}
+solve(5,10)
